79f7fdaede9c6937ee479a6e55c1d56f718f0f10
444Duarte <duartemr.pinto@gmail.com>
2015-10-28 17:22:52 +0000

    Refactoring nos packets de controlo

--- Aula 2/app_layer.c
+++ Aula 2/app_layer.c
@@ -13,9 +13,9 @@
 #include <string.h>
 
 
-void app_layer(LinkLayer *link_layer, char** argv) {
+void app_layer(LinkLayer *link_layer, char* file_name) {
 	if (link_layer->status == TRANSMITTER)
-		app_layer_transmitter(link_layer, argv);
+		app_layer_transmitter(link_layer, file_name);
 	else if (link_layer->status == RECEIVER)
 		app_layer_receiver(link_layer);
 }
@@ -30,41 +30,31 @@ void app_layer_receiver(LinkLayer *link_layer) {
 	//ll_close(link_layer);
 }
 
-void app_layer_transmitter(LinkLayer *link_layer, char** argv) {
+void app_layer_transmitter(LinkLayer *link_layer, char * file_name) {
 
 	int file;
 	char * file_buffer;
-    struct stat file_stat;
+    struct stat * file_stat = mallo(sizeof(struct stat));
 
-    file = open(argv[2], O_RDONLY);
+    file = open(file_name, O_RDONLY);
 
-    fstat(file, &file_stat);
+    fstat(file, file_stat);
 
     file_buffer = malloc(file_stat);
 
     read(file, &file_buffer, file_stat.st_size);
 
-    printf("tamanho!: %lld\n", (long long)file_stat.st_size);
+    printf("tamanho!: %lld\n", (long long)file_stat->st_size);
 
 	//ll_open(link_layer);
 
-	char controlPacketStart[3 + 4 + 3 + strlen(argv[2]) + 1];
+	char controlPacketStart[3 + 4 + 3 + strlen(file_name) + 1];
 	char controlPacketEnd;
 
-	controlPacketStart[0] = 1;
-	controlPacketStart[1] = 0; //tamanho ficheiro
-	controlPacketStart[2] = 4; // 4 bytes é o máximo
-	int i = 0;
-	for(;i < controlPacketStart[2]; i++)
-	{
-		controlPacketStart[i+3] = 0xFF & (file_stat.st_size >> (8 * i));
-	}
-	// 7, a partir de agora é o nome do ficheiro
-	controlPacketStart[3 + controlPacketStart[2]] = 1;
-	controlPacketStart[3 + controlPacketStart[2] + 1] = 1; //nome do ficheiro
-	controlPacketStart[3 + controlPacketStart[2] + 2] = strlen(argv[2]) + 1; //+ 1 porque /0
 
-	memcpy(&controlPacketStart[i + 3 + controlPacketStart[2] + 3], argv[2], strlen(argv[2]) +1);
+	controlPacket_size(file_stat, controlPacket);
+	
+	controlPacket_name(file_stat, &controlPacket[3 + controlPacket[2]], file_name);
 
 	setTries(1);
 	  
@@ -72,4 +62,22 @@ void app_layer_transmitter(LinkLayer *link_layer, char** argv) {
 
 	double cenas = ceil(1.2);
 	printf("t: %f\n", cenas);
+}
+
+void controlPacket_size(struct stat * file_stat, char * controlPacket){
+	controlPacketStart[0] = 0x1;
+	controlPacketStart[1] = 0; //tamanho ficheiro
+	controlPacketStart[2] = 4; // 4 bytes é o máximo
+	int i = 0;
+	for(;i < controlPacketStart[2]; i++){
+		controlPacketStart[i+3] = 0xFF & (file_stat->st_size >> (8 * i));
+	}
+}
+
+void controlPacket_name(struct stat * file_stat, char * controlPacket, char * name){
+	controlPacketStart[0] = 1;
+	controlPacketStart[1] = 1; 					//nome do ficheiro
+	controlPacketStart[2] = strlen(name) + 1; 	//+ 1 porque /0
+
+	memcpy(&controlPacketStart[i + 3 + controlPacketStart[2] + 3], argv[2], strlen(name) +1);
 }
--- Aula 2/app_layer.h
+++ Aula 2/app_layer.h
@@ -3,10 +3,20 @@
 
 #include "utils.h"
 
+#define C_START 1
+#define C_END 2
+
+#define F_SIZE 0
+#define F_NAME 1
+
+#define 
+
 void app_layer(LinkLayer *link_layer, char** argv);
 
 void app_layer_receiver(LinkLayer *link_layer);
 void app_layer_transmitter(LinkLayer *link_layer, char** argv);
 
+void controlPacket_size(struct stat * file_stat, char * controlPacket);
+void controlPacket_name(struct stat * file_stat, char * controlPacket, char * name);
 
 #endif /* ___APPLAYER */
